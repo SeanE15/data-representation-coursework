b6631139970dd1b37466ccd22083b5548b81e25e
Sean Elliott <G00411288@gmit.ie>
2023-10-20 20:42:23 +0100

    continue working on lab

--- Labs/dr4.1.1-urlencode.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse
-query = "Hello World@Python"
-parsed = urllib.parse.quote(query)
-print(parsed)
-
-params = {"q":"Python URL encoding", "as_sitesearch": "www.urlencoder.io"}
-parsedparams = urllib.parse.urlencode(params)
-print(parsedparams)
-
---
+++ lab05.03githubbymodule.py
@@ -0,0 +1,5 @@
+from github import Github
+from config import config as cfg
+
+apikey = cfg["githubkey"]
+
